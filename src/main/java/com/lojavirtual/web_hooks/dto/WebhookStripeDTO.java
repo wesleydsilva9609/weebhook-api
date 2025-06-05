c2111bd4b56c814dc3eb39493dad0920c182f9b6
Wesley D. Silva <wesleydsilva96@gmail.com>
2025-06-05 13:57:09 -0300

    campo type no dto webhook

--- src/main/java/com/lojavirtual/web_hooks/dto/WebhookStripeDTO.java
+++ src/main/java/com/lojavirtual/web_hooks/dto/WebhookStripeDTO.java
@@ -3,7 +3,9 @@ package com.lojavirtual.web_hooks.dto;
 import javax.xml.crypto.Data;
 import java.util.Map;
 
-public record WebhookStripeDTO(Data data) {
+public record WebhookStripeDTO(
+        String type,
+        Data data) {
     public record Data(
             ObjectData object
     ) {}
